e446488a219f2a20cd8908c8b3b78e9ca7475b62
futurebott <52699420+futurebott@users.noreply.github.com>
2020-11-02 22:39:39 -0500

    saving clock

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./styles.css";
+import Clock from "./Clock";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <div>
+        <Clock />
+      </div>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
